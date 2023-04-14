febd364f171a3c053514febedaa44288cde53090
matias landau <landaumatias@gmail.com>
2023-04-14 19:35:06 -0300

    agregado material ui
    Signed-off-by: matias landau <landaumatias@gmail.com>

--- cv-digital/src/App.js
+++ cv-digital/src/App.js
@@ -9,10 +9,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button className="btn btn-primary" type="button">
-          Button
-        </button>
-        Learn React
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
