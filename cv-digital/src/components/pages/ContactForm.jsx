efe1fd1027b39528c1ab59fe1aa77256d2a3b865
matias landau <landaumatias@gmail.com>
2023-05-07 23:18:49 -0300

    agregado formulario de contacto
    Signed-off-by: matias landau <landaumatias@gmail.com>

--- cv-digital/src/components/pages/ContactForm.jsx
+++ cv-digital/src/components/pages/ContactForm.jsx
@@ -1,7 +1,35 @@
+import { makeStyles, TextField, Button } from "@mui/material";
 import React from "react";
+import styles from "./ContactForm.module.css";
 
-const ContactForm = () => {
-  return <div>ContactForm</div>;
-};
+function ContactForm() {
+  return (
+    <form className={`${styles.form}`}>
+      <TextField required id="name" label="Name" variant="outlined" />
+      <TextField
+        required
+        id="email"
+        label="Email"
+        variant="outlined"
+        type="email"
+      />
+      <TextField
+        required
+        id="message"
+        label="Message"
+        variant="outlined"
+        multiline
+        rows={4}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        className={`${styles.button}`}
+      >
+        Send
+      </Button>
+    </form>
+  );
+}
 
 export default ContactForm;
